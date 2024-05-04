b8c6001100e0132addbfbbecf85161cf3ce14ab7
Jessica Sampaio <jessicamsampaio@gmail.com>
2024-05-04 09:22:34 -0300

    primeiro commit

---
+++ src/Teste.tsx
@@ -0,0 +1,38 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || '16px'};
+`
+
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+  span {
+    text-decoration: line-through;
+  }
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao principal fontSize="16px">
+        Enviar
+      </Botao>
+      <Botao fontSize="14px" principal={false}>
+        Cancelar
+      </Botao>
+      <BotaoPerigo as="a" principal fontSize="16px">
+        <span>Não clique aqui</span>
+      </BotaoPerigo>
+    </>
+  )
+}
+
+export default Teste
