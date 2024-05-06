488652a1259ce903236885fa9008c3c26cfca2d0
Jessica Sampaio <jessicamsampaio@gmail.com>
2024-05-06 14:25:33 -0300

    construindo estutura + secao sobre mim

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/piegez.png" />
+    <Titulo fontSize={20}>Jessica Sampaio</Titulo>
+  </aside>
+)
+
+export default Sidebar
