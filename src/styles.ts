220a1b116ab11d4fbad074949cc42ce998198be8
Jessica Sampaio <jessicamsampaio@gmail.com>
2024-05-10 18:53:31 -0300

    finalizacao pagina

--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components'
+import { theme } from './themes/dark'
 
 const EstiloGlobal = createGlobalStyle`
   * {
@@ -11,6 +12,7 @@ const EstiloGlobal = createGlobalStyle`
   body {
     padding-top: 80px;
     padding-bottom: 80px;
+    background-color: ${(props) => (props.theme as theme).corDeFundo};
 
     @media (max-width: 768px) {
       padding-top: 16px;
---
+++ src/themes/dark.ts
@@ -0,0 +1,16 @@
+const temaDark = {
+  corPrincipal: '#fff',
+  corSecundaria: '#eee',
+  corDeFundo: '#282a35',
+  corDeFundoBotao: '#fff',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temaDark
+export type theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFundo: string
+  corDeFundoBotao: string
+  corDaBorda: string
+}
